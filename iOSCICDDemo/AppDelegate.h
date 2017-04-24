8cbc660edce9ab9716c12af32fbec00293985abd
Basawaraj <basawaraj.hiremath@cuelogic.co.in>
2017-04-24 13:20:02 +0530

    first commit

---
+++ iOSCICDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSCICDDemo
+//
+//  Created by Basawaraj on 24/04/17.
+//  Copyright © 2017 Cuelogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSCICDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSCICDDemo
+//
+//  Created by Basawaraj on 24/04/17.
+//  Copyright © 2017 Cuelogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
